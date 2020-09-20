ac6b80d67b606b42c5e856b0b9732e282536ee51
yashwadia7025 <38725886+yashwadia7025@users.noreply.github.com>
2020-09-20 23:36:14 +0530

    Update BlackJackGame.py

--- BlackJackGame.py
+++ BlackJackGame.py
@@ -3,7 +3,7 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
-
+yashwadiaedit
 
 
 class Player:
