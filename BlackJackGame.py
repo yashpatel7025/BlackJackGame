0261ab9e30586e3e3c650fe4d455249503e4914f
yashwadia7025 <38725886+yashwadia7025@users.noreply.github.com>
2020-09-20 23:49:17 +0530

    Update BlackJackGame.py

--- BlackJackGame.py
+++ BlackJackGame.py
@@ -1,10 +1,8 @@
+
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
-
-yashwadiaedit
-
 
 class Player:
                     money=1000
